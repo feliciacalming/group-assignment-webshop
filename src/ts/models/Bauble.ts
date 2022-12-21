2fc4950401142853b423bb764a1e22c57d3bb875
Felicia Calming <felicia.calming@gmail.com>
2022-12-21 15:09:36 +0100

    created list of product objects

---
+++ src/ts/models/Bauble.ts
@@ -0,0 +1,7 @@
+export class Bauble {
+  constructor(
+    public name: string,
+    public image: string,
+    public price: number
+  ) {}
+}
---
+++ src/ts/models/productItems.ts
@@ -0,0 +1,32 @@
+import { Bauble } from "./bauble";
+
+export let productItems: Bauble[] = [
+  {
+    name: "Julgranskula röd",
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/10/18/57/bauble-1814941_1280.png",
+    price: 200,
+  },
+
+  {
+    name: "Julgranskula guld",
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/10/19/01/bauble-1814949_1280.png",
+    price: 280,
+  },
+
+  {
+    name: "Julgranskula blå",
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/10/19/08/bauble-1814972_1280.png",
+    price: 200,
+  },
+  {
+    name: "Julgranskula grön",
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/10/19/10/bauble-1814975_1280.png",
+    price: 200,
+  },
+];
+
+// image: "/assets/bauble-blue.png",
