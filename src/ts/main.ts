cf373495181bf4c96536c4efb491783d558af922
Oskar Sterner <oskar.lr.sterner@gmail.com>
2022-12-23 13:12:23 +0100

    added shoppingcart counter

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,22 @@
+import { ChristmasBauble } from "./models/ChristmasBauble";
 
+export let listFromLocalStorage: ChristmasBauble[] = [];
+
+function displayCounter() {
+    listFromLocalStorage = JSON.parse(localStorage.getItem("product") || "[]");
+
+    let counterContainer: HTMLDivElement = document.createElement("div");
+    let productCounter: HTMLParagraphElement = document.createElement("p");
+
+    productCounter.textContent = listFromLocalStorage.length.toString();
+
+    counterContainer.classList.add("counter");
+    
+    counterContainer.appendChild(productCounter);
+
+    (document.querySelector(".phone-nav") as HTMLElement).appendChild(
+        counterContainer
+    );
+}
+console.log(listFromLocalStorage.length);
+displayCounter();
