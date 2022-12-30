f61e5b26ab009ce964df6fa395487f5eb94c5d8d
Felicia Calming <felicia.calming@gmail.com>
2022-12-30 15:23:31 +0100

    removed unnecessary for loop in productdetails

--- src/ts/productdetails.ts
+++ src/ts/productdetails.ts
@@ -4,14 +4,12 @@ import { productItems } from "./models/productItems";
 import { ProductsInCart } from "./models/ProductsInCart";
 import { addToCart } from "./products";
 
-const assortmentDetailContainer: HTMLDivElement =
-  document.createElement("div");
+const assortmentDetailContainer: HTMLDivElement = document.createElement("div");
 const assortmentDetailHeading: HTMLHeadingElement =
   document.createElement("h2");
 const assortmentDetailInfoText: HTMLParagraphElement =
   document.createElement("p");
-const assortmentAmount: HTMLParagraphElement =
-  document.createElement("h5");
+const assortmentAmount: HTMLParagraphElement = document.createElement("h5");
 
 assortmentDetailHeading.innerHTML = "Julgranskula";
 assortmentDetailInfoText.innerHTML =
@@ -32,44 +30,32 @@ assortmentDetailContainer.appendChild(assortmentAmount);
   assortmentDetailContainer
 );
 
-function displayProductdetails(productItems: ChristmasBauble[]) {
-  for (let i = 0; i < productItems.length; i) {
-    let productContainer: HTMLDivElement =
-      document.createElement("div");
-    let productTitle: HTMLHeadingElement =
-      document.createElement("h5");
-    let productImage: HTMLImageElement =
-      document.createElement("img");
-    let productPrice: HTMLParagraphElement =
-      document.createElement("h5");
-    let productButton: HTMLButtonElement =
-      document.createElement("button");
+function displayProductdetails() {
+  let productContainer: HTMLDivElement = document.createElement("div");
+  let productTitle: HTMLHeadingElement = document.createElement("h5");
+  let productImage: HTMLImageElement = document.createElement("img");
+  let productPrice: HTMLParagraphElement = document.createElement("h5");
+  let productButton: HTMLButtonElement = document.createElement("button");
 
-    productContainer.classList.add("product");
-    productTitle.classList.add("product__title");
-    productImage.classList.add("product__image");
-    productPrice.classList.add("product__price");
-    productButton.classList.add("product__buttonAddToCart");
+  productContainer.classList.add("product");
+  productTitle.classList.add("product__title");
+  productImage.classList.add("product__image");
+  productPrice.classList.add("product__price");
+  productButton.classList.add("product__buttonAddToCart");
 
-    productImage.src = productItems[i].image;
-    productImage.alt = productItems[i].name;
+  productImage.src = productItems[0].image;
+  productImage.alt = productItems[0].name;
 
-    productTitle.innerHTML = productItems[i].name;
-    productPrice.innerHTML = productItems[i].price.toString() + " kr";
-    productButton.innerHTML = "Lägg i varukorg";
+  productTitle.innerHTML = productItems[0].name;
+  productPrice.innerHTML = productItems[0].price.toString() + " kr";
+  productButton.innerHTML = "Lägg i varukorg";
 
-    productContainer.appendChild(productImage);
-    productContainer.appendChild(productTitle);
-    productContainer.appendChild(productPrice);
-    productContainer.appendChild(productButton);
+  productContainer.appendChild(productImage);
+  productContainer.appendChild(productTitle);
+  productContainer.appendChild(productPrice);
+  productContainer.appendChild(productButton);
 
-    (document.querySelector("main") as HTMLElement).appendChild(
-      productContainer
-    );
-
-    productButton.addEventListener("click", () => {
-      addToCart(productItems[i]);
-    });
-  }
+  (document.querySelector("main") as HTMLElement).appendChild(productContainer);
 }
-displayProductdetails(productItems);
+
+displayProductdetails();
