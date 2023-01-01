f4a1f87bd95d34f131c88968cdb2512fd78c8faa
Felicia Calming <felicia.calming@gmail.com>
2023-01-01 23:39:08 +0100

    filter by size-function almost done

--- src/ts/models/filterOptions.ts
+++ src/ts/models/filterOptions.ts
@@ -1,2 +1,9 @@
+export class Sizes {
+  constructor(public size: number, public found: boolean) {}
+}
+
 export const colors: string[] = ["Röd", "Blå", "Rosa", "Grön", "Lila"];
-export const size: number[] = [8, 12];
+export const size: Sizes[] = [
+  { size: 8, found: false },
+  { size: 12, found: false },
+];
--- src/ts/products.ts
+++ src/ts/products.ts
@@ -4,17 +4,16 @@ import { displayCounter } from "./main";
 import { ProductsInCart } from "./models/ProductsInCart";
 import { colors, size } from "./models/filterOptions";
 import { displayProductdetails } from "./productdetails";
+import { toggleFilter } from "./functions.ts/filterFunctions";
 
-const assortmentContainer: HTMLDivElement =
-  document.createElement("div");
-const assortmentHeading: HTMLHeadingElement =
-  document.createElement("h2");
-const assortmentInfoText: HTMLParagraphElement =
-  document.createElement("p");
-const assortmentAmount: HTMLParagraphElement =
-  document.createElement("h5");
-const filterButton: HTMLButtonElement =
-  document.createElement("button");
+const assortmentContainer: HTMLDivElement = document.createElement("div");
+const assortmentHeading: HTMLHeadingElement = document.createElement("h2");
+const assortmentInfoText: HTMLParagraphElement = document.createElement("p");
+const assortmentAmount: HTMLParagraphElement = document.createElement("h5");
+const filterButton: HTMLButtonElement = document.createElement("button");
+
+// const productsOnDisplay: HTMLElement = document.createElement("section");
+// productsOnDisplay.classList.add("productsOnDisplay");
 
 assortmentHeading.innerHTML = "Julgranskulor";
 assortmentInfoText.innerHTML =
@@ -25,12 +24,8 @@ filterButton.innerHTML = "Filter";
 
 //eventlistener för filter
 filterButton.addEventListener("click", () => {
-  let page = document.getElementById("productpage") as HTMLDivElement;
-  page.classList.toggle("hide");
-
-  (
-    document.getElementById("filter") as HTMLDivElement
-  ).classList.toggle("show");
+  toggleFilter();
+  displayProducts(filteredProducts);
 });
 
 assortmentContainer.classList.add("assortment");
@@ -53,16 +48,11 @@ assortmentContainer.appendChild(filterButton);
 
 function displayProducts(productItems: ChristmasBauble[]) {
   for (let i = 0; i < productItems.length; i++) {
-    let productContainer: HTMLDivElement =
-      document.createElement("div");
-    let productTitle: HTMLHeadingElement =
-      document.createElement("h5");
-    let productImage: HTMLImageElement =
-      document.createElement("img");
-    let productPrice: HTMLParagraphElement =
-      document.createElement("h5");
-    let productButton: HTMLButtonElement =
-      document.createElement("button");
+    let productContainer: HTMLDivElement = document.createElement("div");
+    let productTitle: HTMLHeadingElement = document.createElement("h5");
+    let productImage: HTMLImageElement = document.createElement("img");
+    let productPrice: HTMLParagraphElement = document.createElement("h5");
+    let productButton: HTMLButtonElement = document.createElement("button");
 
     productContainer.classList.add("product");
     productTitle.classList.add("product__title");
@@ -81,6 +71,7 @@ function displayProducts(productItems: ChristmasBauble[]) {
     productContainer.appendChild(productTitle);
     productContainer.appendChild(productPrice);
     productContainer.appendChild(productButton);
+    // productsOnDisplay.appendChild(productContainer);
 
     // productContainer.addEventListener("click", () => {
     //   displayProductdetails(productItems[i]);
@@ -94,7 +85,6 @@ function displayProducts(productItems: ChristmasBauble[]) {
 
     productButton.addEventListener("click", () => {
       addToCart(productItems[i]);
-      let test = "hej";
     });
   }
 }
@@ -102,9 +92,7 @@ function displayProducts(productItems: ChristmasBauble[]) {
 export function addToCart(product: ChristmasBauble) {
   //hämta listan från LS här istälet för högst upp i filen
   let listToLocalStorage: ProductsInCart[] = [];
-  listToLocalStorage = JSON.parse(
-    localStorage.getItem("product") || "[]"
-  );
+  listToLocalStorage = JSON.parse(localStorage.getItem("product") || "[]");
 
   let itemToCart: ProductsInCart = new ProductsInCart(1, product);
   let found = false;
@@ -113,14 +101,9 @@ export function addToCart(product: ChristmasBauble) {
     found = false;
   } else {
     for (let i = 0; i < listToLocalStorage.length; i++) {
-      if (
-        listToLocalStorage[i].product.id === itemToCart.product.id
-      ) {
+      if (listToLocalStorage[i].product.id === itemToCart.product.id) {
         listToLocalStorage[i].amount++;
-        localStorage.setItem(
-          "product",
-          JSON.stringify(listToLocalStorage)
-        );
+        localStorage.setItem("product", JSON.stringify(listToLocalStorage));
         console.log("den finns i listan");
         found = true;
         return;
@@ -131,85 +114,113 @@ export function addToCart(product: ChristmasBauble) {
   if (found === false) {
     console.log("hittar inte id");
     listToLocalStorage.push(itemToCart);
-    localStorage.setItem(
-      "product",
-      JSON.stringify(listToLocalStorage)
-    );
+    localStorage.setItem("product", JSON.stringify(listToLocalStorage));
   }
 }
 
 //FILTER FUNKTIONER
-const filterContainer = document.getElementById(
-  "filter"
-) as HTMLDivElement;
-const filterHeading = document.getElementById(
-  "filter-heading"
-) as HTMLElement;
+const filterContainer = document.getElementById("filter") as HTMLDivElement;
+const filterHeading = document.getElementById("filter-heading") as HTMLElement;
 const filterTitle = document.createElement("h1");
 const exitBtn = document.createElement("button");
+const useFilterBtn = document.createElement("button");
+const clearFilterBtn = document.createElement("button");
 
-exitBtn.classList.add("filter__exitButton");
 filterTitle.classList.add("filter__title");
+exitBtn.classList.add("filter__exitButton");
+useFilterBtn.classList.add("filter__filterButton");
+clearFilterBtn.classList.add("filter__clearButton");
 
 filterTitle.innerHTML = "Filter";
 exitBtn.innerHTML = "X";
+useFilterBtn.innerHTML = "Använd filter";
+clearFilterBtn.innerHTML = "Rensa filter";
+
+let filteredProducts: ChristmasBauble[] = [];
 
 // Toggle för Filter-funktionen. När man klickar på X i filter-rutan får den display: none igen, och produktsidan display: block igen.
 exitBtn.addEventListener("click", () => {
-  (
-    document.getElementById("filter") as HTMLDivElement
-  ).classList.toggle("show");
-  (
-    document.getElementById("productpage") as HTMLDivElement
-  ).classList.toggle("hide");
+  toggleFilter();
+});
+
+useFilterBtn.addEventListener("click", () => {
+  toggleFilter();
+  displayProducts(filteredProducts);
 });
 
 filterHeading.appendChild(filterTitle);
 filterHeading.appendChild(exitBtn);
+filterContainer.appendChild(useFilterBtn);
+filterContainer.appendChild(clearFilterBtn);
 
 //FÄRGFILTER.
 
-for (let i = 0; i < colors.length; i++) {
-  let colorFilters: HTMLElement = document.getElementById(
-    "color"
-  ) as HTMLElement;
-  let colorContainer: HTMLDivElement = document.createElement("div");
-  let checkbox: HTMLInputElement = document.createElement("input");
-  checkbox.type = "checkbox";
-  let colorname: HTMLHeadingElement = document.createElement("h4");
+function displayColorOptions() {
+  for (let i = 0; i < colors.length; i++) {
+    let colorFilters: HTMLElement = document.getElementById(
+      "color"
+    ) as HTMLElement;
+    let colorContainer: HTMLDivElement = document.createElement("div");
+    let checkbox: HTMLInputElement = document.createElement("input");
+    checkbox.type = "checkbox";
+    let colorname: HTMLHeadingElement = document.createElement("h4");
 
-  colorContainer.classList.add("color__container");
-  checkbox.classList.add("color__checkbox");
-  colorname.classList.add("color__name");
+    colorContainer.classList.add("color__container");
+    checkbox.classList.add("color__checkbox");
+    colorname.classList.add("color__name");
 
-  colorname.innerHTML = colors[i];
-  colorContainer.appendChild(checkbox);
-  colorContainer.appendChild(colorname);
+    colorname.innerHTML = colors[i];
 
-  colorFilters.appendChild(colorContainer);
+    colorContainer.appendChild(checkbox);
+    colorContainer.appendChild(colorname);
+    colorFilters.appendChild(colorContainer);
+  }
 }
 
 //STORLEKSFILTER.
 
-for (let i = 0; i < size.length; i++) {
-  let sizeFilters: HTMLElement = document.getElementById(
-    "size"
-  ) as HTMLElement;
-  let sizeContainer: HTMLDivElement = document.createElement("div");
-  let checkbox: HTMLInputElement = document.createElement("input");
-  checkbox.type = "checkbox";
-  let sizeNumber: HTMLHeadingElement = document.createElement("h4");
+function displaySizeOptions() {
+  for (let i = 0; i < size.length; i++) {
+    let sizeFilters: HTMLElement = document.getElementById(
+      "size"
+    ) as HTMLElement;
+    let sizeContainer: HTMLDivElement = document.createElement("div");
+    let checkbox: HTMLInputElement = document.createElement("input");
+    checkbox.type = "checkbox";
+    let sizeNumber: HTMLHeadingElement = document.createElement("h4");
 
-  sizeContainer.classList.add("size__container");
-  checkbox.classList.add("size__checkbox");
-  sizeNumber.classList.add("size__number");
+    sizeContainer.classList.add("size__container");
+    checkbox.classList.add("size__checkbox");
+    sizeNumber.classList.add("size__number");
 
-  sizeNumber.innerHTML = size[i].toString();
+    sizeNumber.innerHTML = size[i].size.toString();
 
-  sizeContainer.appendChild(checkbox);
-  sizeContainer.appendChild(sizeNumber);
-  sizeFilters.appendChild(sizeContainer);
+    //eventlyssnare för checkboxen
+
+    checkbox.addEventListener("click", () => {
+      if (checkbox.checked === true) {
+        for (let i = 0; i < productItems.length; i++) {
+          if (sizeNumber.innerHTML === productItems[i].size.toString()) {
+            filteredProducts.push(productItems[i]);
+
+            /* Ska lägga till en knapp för "Använd filter", och när man trycker på den ska den köra toggleFilter för att stänga
+             rutan samt displayProducts med filtrerade listan för att enbart visa de filtrerade produkterna på sidan */
+            // toggleFilter();
+            // displayProducts(filteredProducts);
+          }
+        }
+      }
+    });
+
+    sizeContainer.appendChild(checkbox);
+    sizeContainer.appendChild(sizeNumber);
+    sizeFilters.appendChild(sizeContainer);
+  }
 }
+
+//FILTRERINGSFUNKTION FÖR STORLEK
 
 displayCounter();
 displayProducts(productItems);
+displayColorOptions();
+displaySizeOptions();
